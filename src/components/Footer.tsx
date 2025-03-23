c563dd40d3fa89699cd654f572ccf51711d3891e
Jaya Agarwal <jayaagarwal@bnsal.com>
2025-03-24 01:02:27 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-import { FiTwitter, FiLinkedin, FiInstagram } from 'react-icons/fi';
 
-const FooterContainer = styled.footer`
-  background-color: var(--white);
-  padding: 64px 24px 32px;
-  border-top: 1px solid var(--border-color);
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        {/* Footer content goes here */}
+      </div>
+    </footer>
+  );
+};
 
-  @media (min-width: 1024px) {
-    padding: 80px 80px 40px;
-  }
-`;
+export default Footer;
